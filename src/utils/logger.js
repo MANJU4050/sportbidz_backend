a497613b03063e8bfa11ca3da2d7bd1fddbacda1
Manjunath <makzink40@gmail.com>
2024-02-04 12:02:01 +0530

    winston logger setup

---
+++ src/utils/logger.js
@@ -0,0 +1,46 @@
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, label, prettyPrint } = format;
+
+// Logger for http request logs
+const requestLogger = createLogger({
+  level: "info",
+  format: combine(
+    label({ label: "HTTP request" }),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    prettyPrint()
+  ),
+  transports: [new transports.Console()],
+});
+
+// Logger for database-related logs
+const dbLogger = createLogger({
+  level: "info",
+  format: combine(
+    label({ label: "DatabaseModule" }),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+  ],
+});
+
+const serverLogger = createLogger({
+    level: "info",
+    format: combine(
+      label({ label: "Server state" }),
+      timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+      prettyPrint()
+    ),
+    transports: [
+      new transports.Console(),
+    ],
+  });
+
+
+
+module.exports = {
+  requestLogger,
+  dbLogger,
+  serverLogger
+}
