28a1c0a9d87ba227d3e61ace1f48f27545269127
Manjunath <makzink40@gmail.com>
2024-02-04 11:36:20 +0530

    mongodb setup

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ const express = require("express")
 const {createServer} = require("http")
 const cors = require("cors")
 require("dotenv").config()
+const mongoose = require("mongoose")
 
 const app = express()
 const server = createServer(app)
 
 app.use(cors())
 app.use(express.json())
+
+//connect to mongoDB atlas
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => { console.log("connected to mongoDB") })
+    .catch((error) => { console.error("mongoDB connection failed : ", error) })
 
 app.get('/',async(req,res)=>{
     res.status(200).json({message:"server is up and running"})
