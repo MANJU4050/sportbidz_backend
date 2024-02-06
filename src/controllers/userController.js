a5569c120361055f03965258cb094bb8ce9d39c3
Manjunath <makzink40@gmail.com>
2024-02-06 12:06:35 +0530

    login function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,8 @@
 const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
 
 const User = require("../models/User")
-const { userRegistrationSchema } = require("../validators/userValidator")
+const { userRegistrationSchema, userLoginSchema } = require("../validators/userValidator")
 const { authLogger } = require("../utils/logger")
 
 //user registration
@@ -9,14 +10,11 @@ const registerUser = async (req, res) => {
 
     try {
 
-        console.clear()
-
         authLogger.info(`user registration request by ${req.body.mobile}`)
 
         const { error } = userRegistrationSchema.validate(req.body)
         if (error) {
             authLogger.error(`validation failed with error : ${error.details[0].message}`)
-            console.log(error)
             return res.status(400).json({ error: error.details[0].message });
         }
 
@@ -52,7 +50,45 @@ const registerUser = async (req, res) => {
 
 }
 
+//user login
+const loginUser = async (req, res) => {
+    try {
+
+        const { error } = userLoginSchema.validate(req.body)
+        if (error) {
+            authLogger.error(`validation failed for login by user: ${req.body.mobile}`)
+            return res.status(400).json({ error: error.details[0].message })
+        }
+
+        const user = await User.findOne({ mobile: req.body.mobile })
+        if (!user) {
+            authLogger.error(`no user accounts found for login with mobile : ${req.body.mobile}`)
+            return res.status(401).json({ error: 'user not registered' })
+        }
+
+        const isAuth = await bcrypt.compare(req.body.password, user?.password)
+        if (!isAuth) {
+            authLogger.error(`invalid password login attempt by user : ${req.body.mobile}`)
+            return res.status(401).json({ error: 'invalid credentials' })
+        }
+
+        const accessToken = await jwt.sign({ userId: user?._id }, process.env.ACCESS_TOKEN, { expiresIn: '15m' })
+        const refreshToken = await jwt.sign({ userId: user?._id }, process.env.REFRESH_TOKEN, { expiresIn: '7d' })
+
+        authLogger.info(`successfull login by user : ${req.body.mobile}`)
+        res.cookie('token', accessToken, { httpOnly: true, secure: true, maxAge: 900000 })
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, maxAge: 604800000 }).status(200).json({ message: 'login successfull' })
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({ error: `internal server error` })
+
+    }
+}
+
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
