0501ce157f6e4a00ace0543f438d0f2b6e63cb5a
Manjunath <makzink40@gmail.com>
2024-02-07 15:56:34 +0530

    status check and logout routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,11 @@ const express = require("express")
 const router = express.Router()
 
 const userController = require("../controllers/userController")
+const verifyToken = require('../middlewares/jwt/verifyToken')
 
 router.post('/register', userController.registerUser)
 router.post('/login', userController.loginUser)
+router.get('/checkstatus', verifyToken, userController.userStatusCheck)
+router.delete('/logout', userController.logoutUser)
 
 module.exports = router
