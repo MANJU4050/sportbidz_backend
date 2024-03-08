5a72dd19ef8489d8d44f9436ebe8208398f23743
Manjunath <makzink40@gmail.com>
2024-03-08 12:06:11 +0530

    player model

---
+++ src/models/Player.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const playerSchema = mongoose.Schema({
+    tournamentId: { type: String, required: true },
+    playerName: { type: String, required: true },
+    mobile: { type: String, required: true },
+    playerType: { type: String, required: true },
+    dateOfBirth: { type: Date, },
+    address: { type: String },
+    state: { type: String },
+    district: { type: String },
+    pincode: { type: String },
+    battingStyle: { type: String },
+    bowlingStyle: { type: String },
+    team: { type: String }
+
+})
+
+const Player = mongoose.model('Player', playerSchema)
+
+module.exports = Player
