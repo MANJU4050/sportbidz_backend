af8a97db682391674a85f8221904770430f3d8b5
Manjunath <makzink40@gmail.com>
2024-03-21 17:22:46 +0530

    get auctions by user route

--- src/routes/auctionRoutes.js
+++ src/routes/auctionRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const route = express.Router()
 
-const { auctionRegistration } = require('../controllers/auctionController')
+const { auctionRegistration, getAuctionsByUser } = require('../controllers/auctionController')
 const verifyToken = require('../middlewares/jwt/verifyToken')
 
 route.post('/register', verifyToken, auctionRegistration)
+route.get('/auctions-by-user',verifyToken,getAuctionsByUser)
 
 module.exports = route
